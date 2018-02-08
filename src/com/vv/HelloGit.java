e4b5104431565edae3c3392eed86b259cfa8ee98
vikashverma360 <vikashverma360@gmail.com>
2018-02-08 11:01:36 +0530

    Removed print message other than hello git

--- src/com/vv/HelloGit.java
+++ src/com/vv/HelloGit.java
@@ -14,8 +14,6 @@ public class HelloGit {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello git");
-		System.out.println("Another Repository");
-		System.out.println("Vikash repository");
 	}
 
 }
